dcec0894675ba4c761f11a064359fec729f7b958
WilfredoN <thekillermaps@gmail.com>
2025-07-08 12:57:46 +0300

    feat: scss support

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  
+    sassOptions: {
+      implementation: 'sass-embedded',
+    }
+  
 }
 
 export default nextConfig
