991203d86a46c9a9bf898bd4e910505821090484
WilfredoN <thekillermaps@gmail.com>
2025-07-17 22:01:00 +0300

    feat: base scss variables setup

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next"
+import path from "node:path"
 
 const nextConfig: NextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src")],
+    prependData: `@use './app/variables' as *;`,
+  },
   images: {
     remotePatterns: [
       {
