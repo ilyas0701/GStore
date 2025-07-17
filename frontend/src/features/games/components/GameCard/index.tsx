35d8d4565a70c94f68b3dce68db07313154ae152
WilfredoN <thekillermaps@gmail.com>
2025-07-17 23:44:13 +0300

    feat: wrap GameCard content in Link for improved navigation

--- frontend/src/features/games/components/GameCard/index.tsx
+++ frontend/src/features/games/components/GameCard/index.tsx
@@ -1,24 +1,27 @@
 import type { GameCompact } from "@/features/shared/types/game"
 import Image from "next/image"
+import Link from "next/link"
 import { Badge } from "@/features/shared/components/Badge"
 import "./styles.scss"
 
 export const GameCard = ({ game }: { game: GameCompact }) => {
   return (
-    <div className="store-card">
-      <div className="game-image">
-        <Image
-          src={game.image_url || "/placeholder.png"}
-          alt={game.title}
-          width={260}
-          height={160}
-          priority
-          draggable={false}
-        />
+    <Link href={`/store/${game.id}`}>
+      <div className="store-card">
+        <div className="game-image">
+          <Image
+            src={game.image_url || "/placeholder.png"}
+            alt={game.title}
+            width={260}
+            height={160}
+            priority
+            draggable={false}
+          />
+        </div>
+        <h2>{game.title}</h2>
+        <p>{game.description}</p>
+        <Badge text={`$${game.price}`} />
       </div>
-      <h2>{game.title}</h2>
-      <p>{game.description}</p>
-      <Badge text={`$${game.price}`} />
-    </div>
+    </Link>
   )
 }
