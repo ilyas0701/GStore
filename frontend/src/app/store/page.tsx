887a02e7c9325b84f6642e42a281416f31cb1df0
WilfredoN <thekillermaps@gmail.com>
2025-08-12 13:14:08 +0300

    chore: title for store page

--- frontend/src/app/store/page.tsx
+++ frontend/src/app/store/page.tsx
@@ -1,6 +1,14 @@
+import type { Metadata } from "next"
 import { fetchGames } from "@/features/games/service/games.service"
 import { GameList } from "@/features/store/components/GameList"
 import "./styles.scss"
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Store",
+    description: "Browse and discover new games",
+  }
+}
 
 export default async function StorePage() {
   const games = await fetchGames()
