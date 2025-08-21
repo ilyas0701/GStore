0b3a63b935132d4f67c4ccee6ec9f51fa66da8b1
WilfredoN <thekillermaps@gmail.com>
2025-08-21 15:24:24 +0300

    feat: implement Row component for rendering items in a flexible manner

--- frontend/src/features/games/components/GameGenreRow/index.tsx
+++ frontend/src/features/games/components/GameGenreRow/index.tsx
@@ -1,13 +1,8 @@
 import type { Genre } from "@/features/shared/types/game"
 import { GameGenre } from "@/features/games/components/GameGenre"
+import { Row } from "@/features/shared/components/Table/Row"
 import "./styles.scss"
 
-export const GameGenreRow = ({ genres }: { genres: Genre[] }) => {
-  return (
-    <div className="game-genre-row">
-      {genres.map((genre) => (
-        <GameGenre key={genre.id} {...genre} />
-      ))}
-    </div>
-  )
-}
+export const GameGenreRow = ({ genres }: { genres: Genre[] }) => (
+  <Row items={genres} component={GameGenre} className="game-genre-row" />
+)
